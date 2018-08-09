fbdb3e85c68f7eb33b531a922d4e542a382c7533
Kirill Persionov <kspersionov@gmail.com>
2018-08-09 15:59:46 +0300

    fix price

--- src/components/Price.js
+++ src/components/Price.js
@@ -2,9 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Price = ({ price }) => (
-  <span>
-    {`Цена: ${price}`}
-  </span>
+  <span>Цена {price}</span>
 )
 
 Price.propTypes = {
